77d76316485bf01add711118918a345576a694b0
dhLeoKim <win3ctrl@gmail.com>
2024-05-26 23:44:59 +0900

    0526 springboot study

---
+++ Springboot proj/restful-service/src/main/java/com/example/restfulservice/bean/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package com.example.restfulservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+
+//    public HelloWorldBean(String message) {
+//        this.message = message;
+//    }
+}
